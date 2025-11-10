43105f1095694d7749c6308a77fe4d18cc569b34
Victor Elgersma <elgersmav@gmail.com>
2025-11-10 09:58:05 +0100

    new stuff

---
+++ column_operation.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+df = pd.read_csv("test_data.csv")
+
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_numpy.html
+
+# import test_data
+print(df)
+
+df["one"] = (df["two"] * 4)
